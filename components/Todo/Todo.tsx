bc722e1f6850fa0bc202d97a268f86e00960e944
Viktor Korchagin <viktor.korchagin@kingmidasmedia.ru>
2023-02-23 12:50:04 +0500

    add framer-motion and add create/delete animate

--- components/Todo/Todo.tsx
+++ components/Todo/Todo.tsx
@@ -3,11 +3,20 @@ import {useAppDispatch} from "../../custom-hooks/redux.hooks";
 import styles from './Todo.module.css';
 import {Checkbox} from "../Checkbox/Checkbox";
 import DeleteIcon from './trash-cart-icon.svg';
+import {motion} from 'framer-motion';
+import {useState} from "react";
 
 export const Todo = ({id, text, completed}: ITodo): JSX.Element => {
     const dispatch = useAppDispatch();
+    const [deleteTodos, setDeleteTodos] = useState<boolean>(false);
+
+    const variants = {
+        create: {opacity: 1},
+        delete: {opacity: 0, x: "100%"}
+    }
 
     const onDeleteTodo = (id: string) => {
+        setDeleteTodos(true)
         dispatch(deleteTodo(id));
     }
 
@@ -16,10 +25,15 @@ export const Todo = ({id, text, completed}: ITodo): JSX.Element => {
     }
 
     return (
-        <div className={styles.todo}>
+        <motion.div
+            animate={deleteTodos ? "delete" : "create"}
+            variants={variants}
+            initial={{opacity: 0, height:52}}
+            transition={{duration: 0.5}}
+            className={styles.todo}>
             <Checkbox setToggle={onToggleTodo} completed={completed} id={id}/>
             <p className={styles.text}>{text}</p>
             <DeleteIcon onClick={() => onDeleteTodo(id)} className={styles.delete}/>
-        </div>
+        </motion.div>
     );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/home.module.css';
 import {Todo, TodoForm} from "../../components";
 import {GetServerSideProps} from "next";
 import {wrapper} from "../../store";
+import {motion} from 'framer-motion';
 
 export default function Home(): JSX.Element {
     const todos = useAppSelector((state) => state.todos.todos);
@@ -13,14 +14,16 @@ export default function Home(): JSX.Element {
             <h1 className={styles.h1}>Todos application</h1>
             <div className={styles.todos} >
                 <TodoForm/>
-                {todos.map(todo => (
-                    <Todo
-                        key={todo.id}
-                        id={todo.id}
-                        text={todo.text}
-                        completed={todo.completed}
-                    />
-                ))}
+                <motion.div className={styles.todosWrapper}>
+                    {todos.map(todo => (
+                        <Todo
+                            key={todo.id}
+                            id={todo.id}
+                            text={todo.text}
+                            completed={todo.completed}
+                        />
+                    ))}
+                </motion.div>
             </div>
         </>
 
